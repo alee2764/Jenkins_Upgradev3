7f54813bfc80593db100ee67cfbe6531cd7f3504
alee2764 <alirazaa.mxm@gmail.com>
2023-06-10 09:08:45 +0500

    Update TestGreeter.java

--- Jenkins_Upgradev3-master/maven-samples/single-module/src/test/java/com/example/TestGreeter.java
+++ Jenkins_Upgradev3-master/maven-samples/single-module/src/test/java/com/example/TestGreeter.java
@@ -41,11 +41,4 @@ public class TestGreeter {
 
     assertThat(greeter.greet(someone).length(), is(greaterThan(someone.length())));
   }
-
-    @Test
-  public void greetShouldIncludeGreetingMessageAgain() {
-    String someone = "World, Thanks you making me Happy Again";
-
-    assertThat(greeter.greet(someone).length(), is(greaterThan(someone.length())));
-  }
 }
